d241321e0f69d39ba445d7849039bd2fee1f9b27
ficanikolic <filip.nikolic@nistruct.com>
2024-01-08 20:30:05 +0100

    main: Welcome page finish

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 import Groups from "../../components/Groups";
+import { IUser, initialUser } from "../../models";
+import { getLoggedInUser } from "../../services/Auth";
+import PlaySong from "../../components/PlaySong";
+import SongsSection from "../../components/SongsSection";
 
 const Welcome = () => {
+  const [user, setUser] = useState<IUser>(initialUser);
+
+  useEffect(() => {
+    getLoggedInUser().then((user) => {
+      setUser(user);
+    });
+  });
+
   return (
     <div>
-      <Groups />
+      {user.group ? (
+        <div>
+          <PlaySong groupId={user.group} songLink="" />
+          <SongsSection groupId={user.group} />
+        </div>
+      ) : (
+        <Groups />
+      )}
     </div>
   );
 };
